36431eebd5a1c7b93aceba769fff59c280d263d6
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-28 14:40:38 +0100

    Set Up backend on docker

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const appRoutes = require('./routes/applications');
-const authRoutes = require('./routes/auth');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = 5000;
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Routes
-app.use('/api/applications', appRoutes); // REST dla aplikacji
-app.use('/api/auth', authRoutes);       // REST dla autoryzacji
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
 
-// Start serwera
+app.post('/users', async (req, res) => {
+  const { email, password, githublink, linkedinlink } = req.body;
+  const user = await prisma.user.create({
+    data: { email, password, githublink, linkedinlink }
+  });
+  res.json(user);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
