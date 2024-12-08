dcb2b1f03ff63c934c26a98cfd16a3d59cf414ea
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-12-08 18:31:11 +0100

    Delete trash register

--- client/src/components/register.vue
+++ client/src/components/register.vue
@@ -5,7 +5,7 @@ import * as api from "../API/Api.js";
 export default {
   name: "RegisterPanel",
   setup() {
-    // Reactive variables for form fields and error handling
+  
     const email = ref("");
     const password = ref("");
     const error = ref("");
@@ -32,31 +32,7 @@ export default {
 </script>
 
 
-<!-- <template>
-    <div style="text-align: center; margin: 50px;">
-      <h1>Register</h1>
-  
-      
-      <input
-        type="email"
-        v-model="email"
-        placeholder="Email"
-        style="padding: 10px; width: 300px; margin-bottom: 10px;"
-      />
-      <input
-        type="password"
-        v-model="password"
-        placeholder="Password"
-        style="padding: 10px; width: 300px; margin-bottom: 10px;"
-      />
-  
-      
-      <button @click="handleRegister" style="padding: 10px;">Register</button>
-  
-     
-      <p v-if="error" style="color: red;">{{ error }}</p>
-    </div>
-  </template> -->
+
   
 
   <template>
@@ -96,6 +72,6 @@ export default {
  
   
   <style scoped>
-  /* Stylizacja komponentu (opcjonalnie) */
+  
   </style>
   
