4984b39d737a05603a1bd753927441ba0031c4d7
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-12-19 13:31:23 +0100

    Add Icons

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,24 +1,24 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'addJob') {
-      const applicationsList = document.getElementById('applicationsList');
-      const li = document.createElement('li');
-      li.textContent = `${message.jobTitle}: ${message.jobLink}`;
-      applicationsList.appendChild(li);
-  
-      // Możesz również wysłać dane do swojej aplikacji backendowej
-      fetch('http://localhost:5000/applications', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-        body: JSON.stringify({
-          name: message.jobTitle,
-          link: message.jobLink
-        })
+  if (message.action === 'addJob') {
+    const applicationsList = document.getElementById('applicationsList');
+    const li = document.createElement('li');
+    li.textContent = `${message.jobTitle}: ${message.jobLink}`;
+    applicationsList.appendChild(li);
+
+    // Wysyłanie danych do serwera
+    fetch('http://localhost:5000/applications', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify({
+        name: message.jobTitle,
+        link: message.jobLink
       })
-      .then(response => response.json())
-      .then(data => console.log('Job added:', data))
-      .catch(error => console.error('Error adding job:', error));
-    }
-  });
+    })
+    .then(response => response.json())
+    .then(data => console.log('Job added:', data))
+    .catch(error => console.error('Error adding job:', error));
+  }
+});
