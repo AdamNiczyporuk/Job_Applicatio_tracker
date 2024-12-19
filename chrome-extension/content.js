8754f5011accb5fa65261ec9c7f8c170a4260081
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-12-19 13:36:10 +0100

    Update Manifest

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,12 +1,13 @@
 document.addEventListener('click', function(event) {
-    if (event.target.matches('.b14qiyz3')) { // Zmień selektor na odpowiedni dla przycisku "Aplikuj"
-      const jobTitle = document.querySelector('.cp9trhu').innerText; // Zmień selektor na odpowiedni dla tytułu pracy
-      const jobLink = window.location.href;
-  
-      chrome.runtime.sendMessage({
-        action: 'addJob',
-        jobTitle: jobTitle,
-        jobLink: jobLink
-      });
-    }
-  }, false);
+  if (event.target.matches('.b14qiyz3') || event.target.closest('.b14qiyz3')) { // Zmień selektor na odpowiedni dla przycisku "Aplikuj"
+    const jobTitleElement = document.querySelector('[data-test="text-employerName"]'); // Zmień selektor na odpowiedni dla tytułu pracy
+    const jobTitle = jobTitleElement ? jobTitleElement.innerText : 'Unknown Job Title';
+    const jobLink = window.location.href;
+
+    chrome.runtime.sendMessage({
+      action: 'addJob',
+      jobTitle: jobTitle,
+      jobLink: jobLink
+    });
+  }
+}, false);
