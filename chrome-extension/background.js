506c0f562fd5344dc47373cab798c1cdd64ed71b
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-12-23 10:30:34 +0100

    delete litener from localhost

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,18 +1,18 @@
-let storedToken = null; // Zmienna do przechowywania tokena
+let storedToken = null;
 
 // Obsługa komunikatów
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "saveToken") {
-    storedToken = message.token; // Zapisz token w zmiennej
+    storedToken = message.token;
     console.log("Token zapisany w zmiennej:", storedToken);
     sendResponse({ success: true });
   }
 
   if (message.action === "getToken") {
     console.log("Token pobrany z zmiennej:", storedToken);
-    sendResponse({ token: storedToken }); // Wyślij token do skryptu, który o niego poprosił
+    sendResponse({ token: storedToken }); 
   }
 
 
-  return true; // Wymagane dla asynchronicznych odpowiedzi
+  return true; 
 });
--- chrome-extension/content-localhost.js
+++ chrome-extension/content-localhost.js
@@ -1,40 +1,4 @@
-// // Nasłuchiwanie na załadowanie strony
-// document.addEventListener("DOMContentLoaded", function () {
-//   if (window.location.href === "http://localhost:8080/home") {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       // Przekaż token do background.js
-//       chrome.runtime.sendMessage({ action: "saveToken", token: token }, (response) => {
-//         if (response && response.success) {
-//           console.log("Token został zapisany w background.js:", token);
-//         } else {
-//           console.error("Failed to save token in chrome.storage");
-//         }
-//       });
-//     } else {
-//       console.log("Token not found in localStorage");
-//     }
-//   }
-// });
 
-// document.addEventListener("click", function (event) {
-//   if (event.target.matches('span.v-btn__content') && event.target.textContent.includes('Login')) {
-//     const token = localStorage.getItem('token');
-//     console.log('Token:', token);
-//     if (token) {
-//       // Przekaż token do background.js
-//       chrome.runtime.sendMessage({ action: "saveToken", token: token }, (response) => {
-//         if (response && response.success) {
-//           console.log("Token został zapisany w background.js:", token);
-//         } else {
-//           console.error("Failed to save token in chrome.storage");
-//         }
-//       });
-//     } else {
-//       console.log("Token not found in localStorage");
-//     }
-//   }
-// });
 
 function checkForToken() {
   const token = localStorage.getItem('token');
