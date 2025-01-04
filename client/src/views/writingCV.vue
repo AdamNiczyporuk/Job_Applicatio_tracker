5854cab2bd06063f4dce49caaa4fefcd0a5f160d
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2025-01-04 15:11:08 +0100

    Repair View of the form

--- client/src/views/writingCV.vue
+++ client/src/views/writingCV.vue
@@ -42,47 +42,62 @@ import { useRouter } from 'vue-router';
       <v-btn @click="logout" size="large" variant="outlined" color="red" text>Logout</v-btn>
     </v-col>
   </v-row>
-  <v-container  class="justify-center align-center" style="margin-top: -6vh;">
+
+  <v-row>
+  <v-container  class="justify-center align-center">
     <v-card class="rounded-lg mx-auto"  color="grey-darken-3"  width="75vw" style="overflow: auto;">
       <v-card-title class="text-white ">
         <h2>Fill Data to Generate CV</h2>
       </v-card-title>
-      <v-text-field 
-        label="Job Title" 
-        variant="outlined" 
-        rounded    
-        class="text-white ml-5" max-width="25vw">
-      </v-text-field>
-      <v-text-field 
-        label="Company name" 
-        variant="outlined" 
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field 
+            label="Job Title" 
+            variant="outlined" 
+            rounded    
+            class="text-white ml-5" >
+          </v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field 
+            label="Company name" 
+            variant="outlined" 
+            rounded
+            class="mr-5  text-white">
+          </v-text-field>
+        </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="12">
+        <v-textarea 
+        label="Job description"
+        rows="3"
+        maxlength="700"
+        counter
         rounded
-        class="ml-5  text-white" max-width="25vw">
-      </v-text-field>
-      <v-textarea 
-      width="60vw"
-      label="Job description"
-      maxlength="500"
-      rows="3"
-      counter
-      rounded
-      auto-grow
-      variant="outlined"
-      class="mx-4 mt-2 text-white"
-    ></v-textarea>
-      <v-textarea
-      width="60vw"
-      label="Required Experience"
-      maxlength="300"
-      rows="2"
-      counter
-      auto-grow
-      rounded
-      variant="outlined"
-      class="mx-4 mt-2 text-white"
-    ></v-textarea>
+        auto-grow
+        variant="outlined"
+        class="mx-5  text-white"
+        ></v-textarea>
+    </v-col>
+  </v-row>
+  <v-row> 
+    <v-col cols="12" md="12">
+        <v-textarea
+        label="Required Experience"
+        rows="2"
+        maxlength="500"
+        counter
+        auto-grow
+        rounded
+        variant="outlined"
+        class="mx-5  text-white"
+      ></v-textarea>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12" md="12">
     <v-textarea
-      width="60vw"
       label="Key Values of the Company"
       maxlength="200"
       rows="2"
@@ -92,14 +107,17 @@ import { useRouter } from 'vue-router';
       variant="outlined"
       class="mx-4 mt-2 text-white"
     ></v-textarea>
+    </v-col>
+  </v-row>
     <v-card-actions>
       <v-btn 
       color="green" 
       variant="outlined"
       class="mx-auto">Generate CV</v-btn>
     </v-card-actions>   
-    </v-card>
-  </v-container>
+  </v-card>
+</v-container>
+</v-row>
 </template>
   
   
