4ff45aedbb5ef5733da399f4f43d4b4fab15c7ac
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2025-01-13 11:21:07 +0100

    Fix Bugs

--- client/src/views/writingCV.vue
+++ client/src/views/writingCV.vue
@@ -23,7 +23,7 @@ import { generateCV } from '@/API/GptAPI.js';
       const getCV = async() =>
       {
         if (!PromptData.name || !PromptData.surname || !PromptData.email || !PromptData.jobTitle || !PromptData.company || !PromptData.jobDescription || !PromptData.reqExperience) {
-        ttoast.error("Please fill in all required fields.");
+        toast.error("Please fill in all required fields.");
         return;
         }
         showForm.value = false;
