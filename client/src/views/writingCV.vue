97f6e2179940cd3ae49601bb66856bbf2e8f1f30
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2025-01-23 21:00:36 +0100

    Import function

--- client/src/views/writingCV.vue
+++ client/src/views/writingCV.vue
@@ -2,6 +2,7 @@
 import { ref,reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import * as api from "../API/ServerApi.js";
+import {validateFields} from "../utils/validateFields.js";
   export default {
     name: 'WritingCV',
     setup() {
