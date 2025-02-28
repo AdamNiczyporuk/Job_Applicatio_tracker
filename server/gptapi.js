b4381f45b51746f1b4c3273ad38801e86b7ed988
Adam Niczyporuk <adam.niczyporukk@gmail.com>
2025-02-28 10:42:11 +0100

    SetUpData

--- server/gptapi.js
+++ server/gptapi.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-const openai = new OpenAI('apiKey: process.env.VUE_APP_API_KEY');
+const openai = new OpenAI({apiKey: process.env.VUE_APP_API_KEY});
 
 console.log("API Key:", process.env.VUE_APP_API_KEY);
 
